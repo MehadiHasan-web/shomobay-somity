5ef916107c1ca9e37703d46d09f33a8897e4f286
MH Shakil <mhshakil06@gmail.com>
2024-05-25 18:54:51 +0600

    add talwind css

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,9 @@ const Dashboard = () => {
   return (
     <div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus adipisci ea consequatur. Eos, totam omnis reiciendis maxime sapiente nisi fuga, possimus perferendis nam optio blanditiis hic? Placeat accusamus laudantium exercitationem odit suscipit dolor at, doloremque voluptates corporis. Illo repudiandae similique architecto veritatis reiciendis aspernatur ad veniam sunt maxime nulla obcaecati culpa, esse dignissimos fugiat tempore praesentium sequi explicabo dolore incidunt dolorum. Fuga ipsam, quo minima quam cumque tempora sed expedita similique accusantium, eos voluptate est optio quae inventore, nobis magni voluptatum nisi nam dignissimos doloremque sit unde? Consequatur tempore beatae neque numquam pariatur repellat animi magnam, dicta tenetur nihil quasi.</p>
+      <h1>
+        Hello world!
+      </h1>
     </div>
   );
 };
